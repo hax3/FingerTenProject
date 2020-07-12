afa59550fc1e34c713c402bab1e3a34c8c686d80
JinhwanSeo <dimcon@naver.com>
2020-07-12 23:31:11 +0900

    [INIT] Vue, SpringBoot 연동
    Vue 폴더 내에서 yarn build로 빌드한 후 Server 폴더에서 SpringBoot를 실행

--- Server/src/main/java/com/fingerten/manager/ManagerApplication.java
+++ Server/src/main/java/com/fingerten/manager/ManagerApplication.java
@@ -1,21 +1,17 @@
 package com.fingerten.manager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
+@SpringBootApplication
 public class ManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
 	}
 
-	@GetMapping
-	public String HelloWorld(){
-		return "Hello World!"; 
-	}
 }
